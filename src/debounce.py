e67fad0baa3b5b422af60f94a9f178b9e95f74d4
laurenquarshie <33418406+laurenquarshie@users.noreply.github.com>
2019-05-05 15:51:37 +0100

    Update debounce.py

--- src/debounce.py
+++ src/debounce.py
@@ -6,17 +6,17 @@ GPIO.setup(10, GPIO.IN)
 GPIO.setup(9, GPIO.IN)
 GPIO.setup(11, GPIO.IN) 
 
-def my_callback(10):
+def my_callback(channel):
  print('Callback one')
 
 GPIO.add_event_callback(10, my_callback, bouncetime=200)
 
-def my_callback_two(9):
+def my_callback_two(channel):
     print('Callback two')
 
 GPIO.add_event_callback(9, my_callback, bouncetime=200)
 
-def my_callback_two(11):
+def my_callback_two(channel):
     print('Callback three')
 
 GPIO.add_event_callback(11, my_callback, bouncetime=200)
